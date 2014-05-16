fca0f4606edb3cb40279013c2eab500bb6650cdf
Alex Au <alex.au@athenabest.com>
2014-05-16 17:35:38 +0800

    Upgrade dependencies

--- lib/snapshot_tree/acts_as_tree.rb
+++ lib/snapshot_tree/acts_as_tree.rb
@@ -59,18 +59,18 @@ module SnapshotTree
         instance_variable_set :@tree_helper, TreeHelper.new(options)
 
         has_many :parent_tree_nodes,
+          -> { where(options[:is_active_field].to_sym => true) if options[:is_active_field] },
           class_name:  options[:join_class],
           foreign_key: options[:child_key],
           autosave:    true,
-          dependent:   options[:dependent],
-          conditions:  options[:is_active_field] ? {options[:is_active_field].to_sym => true} : nil
+          dependent:   options[:dependent]
 
         has_many :child_tree_nodes,
+          -> { where(optoins[:is_active_field].to_sym => true) if options[:is_active_field] },
           class_name:  options[:join_class],
           foreign_key: options[:parent_key],
           autosave:    true,
-          dependent:   options[:dependent],
-          conditions:  options[:is_active_field] ? {options[:is_active_field].to_sym => true} : nil
+          dependent:   options[:dependent]
 
         "#{options[:node_prefix]}_depth".to_sym.tap do |field|
           define_method(field) do
